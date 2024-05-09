5bc36a0d9251d46c77113087051ea90c1a0ffbfc
Abdulla-Sirag <3body818@gmail.com>
2024-05-09 23:16:37 +0300

    10 May, 11:15 PM: feat: add shop features.
    - add home search bar.
    - add home categories horizontal list view.
    - add home banners carousal slider.

--- lib/common/widgets/images/isma_rounded_image.dart
+++ lib/common/widgets/images/isma_rounded_image.dart
@@ -1,6 +1,5 @@
 
 import 'package:flutter/material.dart';
-import '../../../utils/constants/Image_strings.dart';
 import '../../../utils/constants/colors.dart';
 import '../../../utils/constants/sizes.dart';
 
